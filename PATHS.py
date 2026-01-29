fd1e74d8a40cab3d30c39a22e8a33fc4c0471b45
Onkar Kadam <kadamonkar2003@gmail.com>
2026-01-29 16:34:22 +0530

    Add files via upload

---
+++ PATHS.py
@@ -0,0 +1,12 @@
+import warnings
+warnings.filterwarnings('ignore')
+NAVBAR_PATHS = {
+    'HOME':'home',
+    'ABOUT': 'about',
+    'INTRODUCTION': 'introduction'
+}
+
+SETTINGS = {
+    'OPTIONS':'options',
+    'CONFIGURATION':'configuration'
+}
